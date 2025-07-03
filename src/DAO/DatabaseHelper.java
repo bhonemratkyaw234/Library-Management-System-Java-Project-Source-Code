413ad840193d43b61fb5f8a97999915ccb81e145
bhonemratkyaw234 <bounmyat396@gmail.com>
2025-07-03 10:57:08 +0630

    project file

--- src/DAO/DatabaseHelper.java
+++ src/DAO/DatabaseHelper.java
@@ -1,15 +1,16 @@
-
 package DAO;
 
 import java.sql.*;
 public class DatabaseHelper {
     public static Connection getConnection(){
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/library","root","selena007gomax");
+            // Correct driver class for MySQL 8+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Make sure database name is correct
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/library_management","root","selena007gomax");
             return con;
         } catch (Exception e) {
-            System.err.println("Connection error");
+            System.err.println("Connection error: " + e.getMessage());
             return null;
         }
     }
